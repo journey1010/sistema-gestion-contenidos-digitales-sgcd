73da3d125c2a5631c96899c36e77a6e2f0861096
Journii <ginopaflo001608@gmail.com>
2024-02-29 12:40:04 -0500

    Seeder for users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -11,7 +13,13 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        
+    {   
+        DB::table('users')->insert([
+            'name' => 'Pedrito Electrocutado',
+            'email' => 'tilintester@gmail.com',
+            'password' => Hash::make('Hola5.2'),
+            'rol' => '1',
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
