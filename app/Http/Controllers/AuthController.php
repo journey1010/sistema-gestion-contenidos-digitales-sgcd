ae23a2072f24ffd1dac1ec34c06440e0c5945871
Journii <ginopaflo001608@gmail.com>
2024-03-01 12:57:18 -0500

    Modify AuthController Response it agrees userId

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,55 +15,56 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except'=> ['login', 'refreshToken']]);
+      $this->middleware('auth', ['except'=> ['login', 'refreshToken']]);
     }
 
     public function login(LoginRequest $request)
     {
-        $credentials = $request->only('email', 'password');
-        if (!$token = Auth::guard('api')->attempt($credentials)) {
-            return response()->json([
-                'status'=>'error', 
-                'message' => 'Unauthorized'
-            ], 401);
-        }
-    
-        $user = Auth::guard('api')->user();
-        $token = JWTAuth::claims(['type' => $user->rol])->fromUser($user);
-        
-        return response()->json([
-            'status' => 'success',
-            'token' => $token,
-        ], 200);
+      $credentials = $request->only('email', 'password');
+      if (!$token = Auth::guard('api')->attempt($credentials)) {
+          return response()->json([
+              'status'=>'error', 
+              'message' => 'Unauthorized'
+          ], 401);
+      }
+  
+      $user = Auth::guard('api')->user();
+      $token = JWTAuth::claims(['type' => $user->rol])->fromUser($user);
+      
+      return response()->json([
+          'status' => 'success',
+          'token' => $token,
+          'userId' => $user->id,
+      ], 200);
     }
 
     public function refreshToken()
     {
-        try {
-            $oldToken = JWTAuth::parseToken();
-          
-            $email = $oldToken->getPayload()->get('email');
-            $user = User::where('email', $email)->first();
-            if (!$user || !$user->status) {
-              return response()->json([
-                'status' => 'error',
-                'message' => "Esta cuenta de usuario, esta Inactiva",
-              ], 401);
-            }
-          
-            $newToken = JWTAuth::fromUser($user, [], true);
-          
-            return response()->json([
-              'status' => 'success',
-              'token' => $newToken,
-            ], 200);
-            
-          } catch (\Exception $e) {
+      try {
+          $oldToken = JWTAuth::parseToken();
+        
+          $email = $oldToken->getPayload()->get('email');
+          $user = User::where('email', $email)->first();
+          if (!$user || !$user->status) {
             return response()->json([
               'status' => 'error',
-              'message' => 'Ocurrio un problema al generar al token',
+              'message' => "Esta cuenta de usuario, esta Inactiva",
             ], 401);
           }
+        
+          $newToken = JWTAuth::fromUser($user, [], true);
+        
+          return response()->json([
+            'status' => 'success',
+            'token' => $newToken,
+          ], 200);
+          
+        } catch (\Exception $e) {
+          return response()->json([
+            'status' => 'error',
+            'message' => 'Ocurrio un problema al generar al token',
+          ], 401);
+        }
     }
 
     public function register(RegisterRequest $request)
@@ -78,7 +79,7 @@ class AuthController extends Controller
         return response()->json([
           'status' => 'success',
           'message' => 'Nuevo cuenta de usuario agregada',
-        ], 200);         
+        ], 200);       
       } catch(\Exception $e){
         return response()->json([
           'status' => 'error',
