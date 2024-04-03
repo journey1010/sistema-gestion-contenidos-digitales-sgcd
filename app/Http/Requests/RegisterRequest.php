d310d1473ba0d7802398d9ec76c2b86087fc48b5
Journii <ginopaflo001608@gmail.com>
2024-04-02 22:28:14 -0500

    RegisterRequest Finisehd

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,15 +3,31 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+use App\Rules\PasswordVerify;
 
 class RegisterRequest extends FormRequest
 {
+    protected $stopOnFirstFailure = true;
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $jsonResponse = new JsonResponse([
+            'status' => 'error',
+            'message' => messageValidation($validator),
+        ], 422);
+        
+        throw new HttpResponseException($jsonResponse);
     }
 
     /**
@@ -22,7 +38,20 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => ['required', new PasswordVerify],
+            'rol' => 'required|string'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Un nombre completo es requerido',
+            'email.required' =>  'Un email es requerido',
+            'password.required' => 'Una contraseña es requerida',
+            'rol.required' => 'un rol es requerido'
         ];
     }
 }
